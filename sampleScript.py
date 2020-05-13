bc1c84b350d792fc52fe549acd79b5e8a354c5ed
bshabashFD <bshabash007@outlook.com>
2020-05-13 11:11:12 -0700

    restored first version of python script but removed the final output

--- sampleScript.py
+++ sampleScript.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 my_matrix = np.eye(4)
 my_second_matrix = np.array[[1,2,3,4],
-                            [5,4,3,2],
-                            [4,0,9,8]]
+                            [2,3,4,5],
+                            [4,0,9,8],
+                            [2,3,6,6]]
 
 print(my_matrix)
 print(my_second_matrix)
 print("hello")
-print("byeeeeeeee")
