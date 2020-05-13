0ea4c8b50c6b0cbcd4e8fd5e851870b0cfd27623
bshabashFD <bshabash007@outlook.com>
2020-05-13 10:44:18 -0700

    changed my second matrix

--- sampleScript.py
+++ sampleScript.py
@@ -3,9 +3,8 @@ import pandas as pd
 
 my_matrix = np.eye(4)
 my_second_matrix = np.array[[1,2,3,4],
-                            [2,3,4,5],
-                            [4,0,9,8],
-                            [2,3,6,6]]
+                            [5,4,3,2],
+                            [4,0,9,8]]
 
 print(my_matrix)
 print(my_second_matrix)
